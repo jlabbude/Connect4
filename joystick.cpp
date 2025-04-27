7258ee7f206225adc01f773eb01d107d55d348ad
Lucas <131266818+jlabbude@users.noreply.github.com>
2025-04-27 09:00:29 -0300

    Update joystick.cpp

--- joystick.cpp
+++ joystick.cpp
@@ -20,18 +20,9 @@ void JoyStick::setup_joystick()
 
 StickStatus JoyStick::handle_movements()
 {
-    if (!digitalRead(button_click))
-    {
-        Serial.println("click");
-        return StickStatus::Click;
-    }
-    return handle_single_stick(analogRead(stick_x) * (5.0 / 1023.0));
-    // return (!digitalRead(button_click)) ? StickStatus::Click : handle_single_stick(analogRead(stick_x) * (5.0 / 1023.0));
+    return (!digitalRead(button_click)) ? StickStatus::Click : handle_single_stick(analogRead(stick_x) * (5.0 / 1023.0));
 }
 
-// static = 1.62
-//  negative = 3.33
-//  positive = 0
 StickStatus JoyStick::handle_single_stick(float stick_val)
 {
     String name;
@@ -48,21 +39,11 @@ StickStatus JoyStick::handle_single_stick(float stick_val)
         break;
     }
     if (stick_val > negative_movement_voltage)
-    {
-        Serial.print(name);
-        Serial.println(" moved left");
         return StickStatus::NegativeMovement;
-    }
     else if (stick_val < 1)
-    {
-        Serial.print(name);
-        Serial.println(" moved right");
         return StickStatus::PositiveMovement;
-    }
     else
-    {
         return StickStatus::StaticMovement;
-    }
 }
 
 PType JoyStick::to_ptype()
